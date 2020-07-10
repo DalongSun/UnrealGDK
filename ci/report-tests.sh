23b53efe7c35c288207736479d3414cf60cf4445
Jessica Falk <jessica@improbable.io>
2020-07-10 10:49:53 +0100

    grab the log

--- ci/report-tests.sh
+++ ci/report-tests.sh
@@ -42,12 +42,14 @@ pushd "$(dirname "$0")"
     fi
 
     # Upload artifacts to Buildkite, capture output to extract artifact ID in the Slack message generation
-    UPLOAD_OUTPUT=$(buildkite-agent artifact upload "${TEST_RESULTS_DIRECTORY}/*")
+    buildkite-agent artifact upload "${TEST_RESULTS_DIRECTORY}/*" > bk_log.tmp
+    UPLOAD_OUTPUT=$(cat bk_log.tmp)
     if [ $? -ne 0 ]; then
         echo "Failed to upload artifact."
         exit 1
     fi
 
+    echo "${UPLOAD_OUTPUT}"
     # Artifacts are assigned an ID upon upload, so grab IDs from upload process output to build the artifact URLs
     # The output log is: "Uploading artifact <artifact id> <upload path>". We are interested in the artifact id
     REGEX="[^ ]* ${TEST_RESULTS_DIRECTORY}/index.html.*[^ ]* ${TEST_RESULTS_DIRECTORY}/tests.log"
